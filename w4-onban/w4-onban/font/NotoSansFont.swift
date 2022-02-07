b05b3585660fc13126a3fe4ceb3d2b242d8f2ffd
nylah-j <nylah.j@kakaoent.com>
2022-02-07 17:40:18 +0900

    [FEAT] font 파일 추가

---
+++ w4-onban/w4-onban/font/NotoSansFont.swift
@@ -0,0 +1,39 @@
+////
+////  NotoSansFont.swift
+////  w4-onban
+////
+////  Created by nylah.j on 2022/02/04.
+////
+//
+//import Foundation
+//import UIKit
+//
+//enum NotoSansFileName: String {
+//    case bold = "NotoSansKR-Bold"
+//    case regular = "NotoSansKR-Regular"
+//}
+//class NotoSansFont: UIFont {
+//
+//    override class func systemFont(ofSize fontSize: CGFloat, weight: UIFont.Weight) -> UIFont {
+//        switch weight {
+//        case .bold
+//
+//        default:
+//            return super.init(name: NotoSansFileName.regular.rawValue, size: fontSize)
+//        }
+//    }
+//
+//    override init(ofSize: String, )
+//
+//    convenience init(weight: NotoSansFileName, size: CGFloat) {
+//        let weight = weight ?? .regular
+//        super.init(name: weight.rawValue, size: size)
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        coder.
+//        let weight =
+//    }
+//
+//    UIFont.Weight
+//}
