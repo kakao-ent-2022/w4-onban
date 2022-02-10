a0035681eeb5cb665a045389986a9b059576015a
nylah-j <nylah.j@kakaoent.com>
2022-02-10 12:54:40 +0900

    [FEAT] title 짤리지 않도록 수정

--- w4-onban/w4-onban/view/main/FoodCell.swift
+++ w4-onban/w4-onban/view/main/FoodCell.swift
@@ -24,6 +24,7 @@ class FoodCell: UICollectionViewCell {
         let view = UILabel()
         view.font = UIFont(onbanStyle: .bold, size: 16)
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.numberOfLines = 0
         return view
     }()
     
